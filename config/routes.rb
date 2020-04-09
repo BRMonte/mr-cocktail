45b92426faa7db2bd8fcc81d61e70fcfd942bd57
bruno montebello <brunormontebello@hotmail.com>
2020-04-08 21:04:15 -0300

    Create model Review and its controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'reviews/create'
   get 'cocktails/index'
   get 'cocktails/show'
   get 'cocktails/new'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_08_234423) do
+ActiveRecord::Schema.define(version: 2020_04_09_000246) do
 
   create_table "cocktails", force: :cascade do |t|
     t.string "name"
@@ -32,6 +32,15 @@ ActiveRecord::Schema.define(version: 2020_04_08_234423) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.text "content"
+    t.integer "rating"
+    t.integer "cocktail_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["cocktail_id"], name: "index_reviews_on_cocktail_id"
   end
 
 end
