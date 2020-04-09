8e5939c497ed99f0bf85f20a5372fd18a7b8465b
bruno montebello <brunormontebello@hotmail.com>
2020-04-08 21:17:53 -0300

    Add cocktail actions and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
+
   get 'reviews/create'
-  get 'cocktails/index'
-  get 'cocktails/show'
-  get 'cocktails/new'
-  get 'cocktails/create'
+
+  resources :cocktails, only: [:index, :show, :new, :create] do
+    resources :doses, only: [:create]
+    resources :reviews, only: [:create]
+  end
+
   root to: "cocktails#index"
+
+  resources :doses, only: [:destroy]
+
+
 end
