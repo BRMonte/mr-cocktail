76488aae6be956d85e669f84fff41b70b01dc6e0
bruno montebello <brunormontebello@hotmail.com>
2020-04-08 21:28:27 -0300

    Add doses controller  actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
 
-  get 'reviews/create'
-
   resources :cocktails, only: [:index, :show, :new, :create] do
     resources :doses, only: [:create]
     resources :reviews, only: [:create]
